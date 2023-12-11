ef1f04844903c43aa2dee1beeb2f25bc409acf02
muudar <mohammedmudher@hotmail.com>
2023-12-11 21:27:41 +0200

    finished project

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { messages: messages });
+});
+
+router.get("/new", function (req, res, next) {
+  res.render("form");
+});
+
+router.post("/new", function (req, res, next) {
+  let messageText = req.body.messageText || "";
+  let author = req.body.author || "";
+  messages.push({ text: messageText, user: author, added: new Date() });
+  res.redirect("/");
 });
 
 module.exports = router;
